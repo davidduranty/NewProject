5785c3e1707983da5b44cfa35e499dc6d605075a
David Duranty <dadaduranty@gmail.com>
2023-11-27 01:10:10 +0100

    add img home

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <>
       <Navigation />
-      <Footer />
 
       <div>
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ContentImg from "../components/pictureContainer/ContentImg";
 
 function Home() {
-  return;
+  return <ContentImg />;
 }
 
 export default Home;
