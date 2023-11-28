e79e90e198c1e63c9108c5fe64810ecec4ce96a8
David Duranty <dadaduranty@gmail.com>
2023-11-28 22:50:47 +0100

    add img 3

--- src/components/pictureContainer/ContentImg.jsx
+++ src/components/pictureContainer/ContentImg.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import imageone from "../../styles/img/img1.png";
 import imagetwo from "../../styles/img/img2.png";
+import imagethree from "../../styles/img/img3.png";
 const ContentImg = () => {
   return (
     <div className="content-container">
@@ -21,7 +22,8 @@ const ContentImg = () => {
           <button type="submit">NOUVELLES R&#201;COLTES</button>
         </div>
       </div>
-      <div className="img3">
+      <div className="img1">
+        <img src={imagethree} alt="" />
         <div className="grid-1">
           <h1>FLEURS BLANCHES</h1>
           <button type="submit">D&#201;COUVRIR</button>
