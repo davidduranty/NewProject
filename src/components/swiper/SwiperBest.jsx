f07811fc4be0dd95913e63d1e21b139f12b34109
David Duranty <dadaduranty@gmail.com>
2023-11-30 19:14:25 +0100

    add swiper meiileurs ventes

--- src/components/swiper/SwiperBest.jsx
+++ src/components/swiper/SwiperBest.jsx
@@ -3,6 +3,13 @@ import React, { useEffect, useRef, useState } from "react";
 import Card from "../cards/Card";
 import { useApi } from "../../data/Context";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/scss";
+import "swiper/scss/pagination";
+
+// import "./styles.scss";
+
 const SwiperBest = () => {
   const { tea, getData, getDataFilter } = useApi();
   const listRef = useRef();
@@ -40,7 +47,7 @@ const SwiperBest = () => {
   // };
 
   return (
-    <div>
+    <>
       <div className="title">
         <h1>MEILLEURES VENTES DU MOIS</h1>
         <img src="search.png" alt="" />
@@ -54,16 +61,42 @@ const SwiperBest = () => {
         {/* <span className="left" onClick={() => scrollToImage("next")}>
           &#8678;
         </span> */}
-        <ul className="caroussel" ref={listRef}>
-          {tea.map((tea) => (
-            <Card tea={tea} key={tea.id} />
-          ))}
-        </ul>
-        {/* <span className="right" onClick={() => scrollToImage("prev")}>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <ul className="caroussel" ref={listRef}>
+            <SwiperSlide>
+              {tea.map((tea) => (
+                <Card tea={tea} key={tea.id} />
+              ))}
+            </SwiperSlide>
+          </ul>
+          {/* <span className="right" onClick={() => scrollToImage("prev")}>
           &#8680;
         </span> */}
+        </Swiper>
       </div>
-    </div>
+    </>
   );
 };
 
