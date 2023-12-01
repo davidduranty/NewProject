969534a1fc9faa1c4da17d709671ae87fcf9004d
David Duranty <dadaduranty@gmail.com>
2023-12-02 00:41:00 +0100

    add content glace

---
+++ src/components/pictureContainer/TitleImgTextIce.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TitleImgTextIce() {
+  return (
+    <>
+      <div className="smallBreakfast">
+        <h1>L'ETE GLACE</h1>
+        <img src="petit-dej.webp" alt="petit dej" />
+      </div>
+      <div className="textContainer">
+        <p>
+          Faîtes de votre petit-déjeuner le meilleur moment de la journée.
+          Découvrez notre sélection de thés au caractère corsé, qui peuvent être
+          dégustés nature ou avec du lait pour une tasse plus onctueuse.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TitleImgTextIce;
--- src/pages/Glace.jsx
+++ src/pages/Glace.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useEffect } from "react";
+import TitleImgTextIce from "../components/pictureContainer/TitleImgTextIce";
 
 function Glace() {
-  return <div>Glace</div>;
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        document.querySelector(".arrow-fixed").classList.add("active");
+      } else {
+        document.querySelector(".arrow-fixed").classList.remove("active");
+      }
+    });
+  }, []);
+  return <TitleImgTextIce />;
 }
 
 export default Glace;
