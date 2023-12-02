1792bd44c41eb50a503d8697023898dd7af0a30f
David Duranty <dadaduranty@gmail.com>
2023-12-02 01:17:24 +0100

    add container smallDej

--- src/components/cards/CardSmallDej.jsx
+++ src/components/cards/CardSmallDej.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function CardSmallDej() {
-  return <div>CardSmallDej</div>;
-}
+const TassesSmallDej = () => {
+  const [getTasse, setGetTasse] = useState([]);
+  const getDataTasse = () => {
+    axios.get("http://localhost:5172/dej").then((res) => setGetTasse(res.data));
+  };
+  useEffect(() => {
+    getDataTasse();
+  }, []);
+  return (
+    <div className="container-breakfast">
+      <img src="photo-dej.webp" alt="Petit dej" />
+      <div className="tasse-map">
+        {getTasse.map((tasse) => (
+          <ul className="ul-settings" key={tasse.id}>
+            <img src={tasse.img} alt="Tasse Doman" />
+            <h1>{tasse.name}</h1>
+            <h2>{tasse.content}</h2>
+            <p>{tasse.price.toFixed(2)} £</p>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CardSmallDej;
+export default TassesSmallDej;
