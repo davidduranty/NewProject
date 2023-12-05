2ed71d4758f0d42568c231f6adb84cdf34690bd0
David Duranty <dadaduranty@gmail.com>
2023-12-05 01:28:05 +0100

    add functionality connections

--- src/components/navbar/Navigation.jsx
+++ src/components/navbar/Navigation.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Icons from "./Icons";
+import TeaInfusion from "../liens-navbar/TeaInfusion";
+import BoiteSachets from "../liens-navbar/BoiteSachets";
+import CoffretKdo from "../liens-navbar/CoffretKdo";
+import Accessoires from "../liens-navbar/Accessoires";
+import FamillyTeas from "../liens-navbar/FamillyTeas";
 
 function Navigation() {
+  const [showInfusion, setShowInfusion] = useState(false);
+  const [showBox, setShowBox] = useState(false);
+  const [showPresent, setShowPresent] = useState(false);
+  const [showAccessoiries, setShowAccessoiries] = useState(false);
+  const [showFamily, setShowFamily] = useState(false);
   return (
     <div className="navigation-container">
       <Logo />
       <ul className="ul">
-        <li className="li">THÉS & INFUSIONS</li>
-        <li className="li">BOITES & SACHETS</li>
-        <li className="li">COFFRETS & CADEAUX</li>
-        <li className="li">ACCESSOIRES</li>
+        <li className="li" onClick={() => setShowInfusion(!showInfusion)}>
+          THÉS & INFUSIONS
+        </li>
+        <li className="li" onClick={() => setShowBox(!showBox)}>
+          BOITES & SACHETS
+        </li>
+        <li className="li" onClick={() => setShowPresent(!showPresent)}>
+          COFFRETS & CADEAUX
+        </li>
+        <li
+          className="li"
+          onClick={() => setShowAccessoiries(!showAccessoiries)}
+        >
+          ACCESSOIRES
+        </li>
         <li className="li">GOURMANDISES</li>
-        <li className="li">DOMAN</li>
+        <li className="li" onClick={() => setShowFamily(!showFamily)}>
+          DOMAN
+        </li>
+        {showInfusion && <TeaInfusion />}
+        {showBox && <BoiteSachets />}
+        {showPresent && <CoffretKdo />}
+        {showAccessoiries && <Accessoires />}
+        {showFamily && <FamillyTeas />}
       </ul>
       <Icons />
     </div>
