4d136e571f64d4724d0c53d14fb7c825a8b1cbbb
David Duranty <dadaduranty@gmail.com>
2023-12-06 13:59:22 +0100

    add compmonents form

--- src/components/navbar/Icons.jsx
+++ src/components/navbar/Icons.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import Form from "./login/Form";
 function Icons() {
+  const [open, setOpen] = useState(false);
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handlers = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("click", handlers);
+    return () => {
+      document.removeEventListener("click", handlers);
+    };
+  }, []);
   return (
-    <div className="icons-container">
-      <img src="user.png" alt="" />
+    <div className="icons-container" ref={menuRef}>
+      <img src="user.png" alt="" onClick={() => setOpen(!open)} />
       <img src="favorite.png" alt="" />
       <img src="bag.png" alt="" />
+      {open && <Form />}
     </div>
   );
 }
