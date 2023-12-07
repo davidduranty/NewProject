eff3109e5c2d9fa2ce7bc9fc349abb8b249dec03
David Duranty <dadaduranty@gmail.com>
2023-12-07 22:43:07 +0100

    add route decouverte and title and img

--- src/pages/Decouverte.jsx
+++ src/pages/Decouverte.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import imagefour from "../../src/styles/img/img4.png";
+import { Link } from "react-router-dom";
 
 function Decouverte() {
-  return <div>Decouverte</div>;
+  return (
+    <>
+      <div className="smallBreakfast">
+        <Link to="/home">
+          <p>Accueil</p>
+        </Link>
+        <h1>DECOUVERTE</h1>
+        <img src={imagefour} alt="petit dej" />
+      </div>
+      <div className="textContainer">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          culpa illum veritatis tempora vitae perspiciatis praesentium porro
+          possimus, recusandae laboriosam repellat dignissimos voluptatem
+          consequatur laborum error nisi vel beatae ducimus facilis, quibusdam a
+          cum qui?
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Decouverte;
