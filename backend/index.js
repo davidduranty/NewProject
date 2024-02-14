dd97876d2fbc9c96ec87806d30ed758e7d004139
David Duranty <dadaduranty@gmail.com>
2024-02-14 14:26:37 +0100

    add Back config and start responsive disgn

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = parseInt(process.env.APP_PORT ?? "6000", 10);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Quelque chose de mauvais s'est produit");
+  } else {
+    console.log(`Le serveur écoute sur le port ${port}`);
+  }
 });
--- src/components/pictureContainer/ContentImg.jsx
+++ src/components/pictureContainer/ContentImg.jsx
@@ -4,30 +4,32 @@ import imagetwo from "../../styles/img/img2.png";
 import imagethree from "../../styles/img/img3.png";
 const ContentImg = () => {
   return (
-    <div className="content-container">
-      <div className="img1">
-        <img src={imageone} alt="" />
-        <div className="grid-1">
-          <h1>L &#201;T&#201; GLAC&#201;</h1>
-          <Link to="/glace">
+    <div className="container-response">
+      <div className="content-container">
+        <div className="img1">
+          <img src={imageone} alt="photo été glacé" />
+          <div className="grid-1">
+            <h1>L &#201;T&#201; GLAC&#201;</h1>
+            <Link to="/glace">
+              <button type="submit">D&#201;COUVRIR</button>
+            </Link>
+          </div>
+        </div>
+        <div className="img1">
+          <img src={imagetwo} alt="" />
+          <div className="grid-1">
+            <h1>GRANDS CRUS</h1>
+            <Link to="/decouverte">
+              <button type="submit">NOUVELLES R&#201;COLTES</button>
+            </Link>
+          </div>
+        </div>
+        <div className="img1">
+          <img src={imagethree} alt="" />
+          <div className="grid-1">
+            <h1>FLEURS BLANCHES</h1>
             <button type="submit">D&#201;COUVRIR</button>
-          </Link>
-        </div>
-      </div>
-      <div className="img1">
-        <img src={imagetwo} alt="" />
-        <div className="grid-1">
-          <h1>GRANDS CRUS</h1>
-          <Link to="/decouverte">
-            <button type="submit">NOUVELLES R&#201;COLTES</button>
-          </Link>
-        </div>
-      </div>
-      <div className="img1">
-        <img src={imagethree} alt="" />
-        <div className="grid-1">
-          <h1>FLEURS BLANCHES</h1>
-          <button type="submit">D&#201;COUVRIR</button>
+          </div>
         </div>
       </div>
     </div>
