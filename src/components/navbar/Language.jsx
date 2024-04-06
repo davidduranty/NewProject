73a5014a0b291af4d78e24efa1937a8e892f645f
David Duranty <dadaduranty@gmail.com>
2024-04-06 13:45:37 +0200

    front: add ternere and fix style btn language

--- src/components/navbar/Language.jsx
+++ src/components/navbar/Language.jsx
@@ -1,21 +1,29 @@
-// import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 import i18n from "../../I18n";
 
 function Language() {
-  //   const { t } = useTranslation();
-
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
+
+  useEffect(() => {
+    changeLanguage("fr");
+  }, []);
   return (
     <div className="language-container">
-      <button className="btn-en" onClick={() => changeLanguage("en")}>
+      <button
+        className={`btn-en ${i18n.language === "en" ? "active" : ""}`}
+        onClick={() => changeLanguage("en")}
+      >
         EN
       </button>
-      <button className="btn-fr" onClick={() => changeLanguage("fr")}>
+      <span>|</span>
+      <button
+        className={`btn-fr ${i18n.language === "fr" ? "active" : ""}`}
+        onClick={() => changeLanguage("fr")}
+      >
         FR
       </button>
-      {/* <li>{t("boxes and bags")}</li> */}
     </div>
   );
 }
