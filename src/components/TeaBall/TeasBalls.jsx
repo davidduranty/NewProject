99f8de677edd608e85f8212481990543324bb6d5
David Duranty <dadaduranty@gmail.com>
2024-04-07 19:38:23 +0200

    Front: add translation TeasBalls component

--- src/components/TeaBall/TeasBalls.jsx
+++ src/components/TeaBall/TeasBalls.jsx
@@ -1,58 +1,62 @@
+import { useTranslation } from "react-i18next";
+
 const TeasBalls = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="teas-balls-container">
       <div className="img">
         <img src="Thé-noir.avif" alt="" />
         <div className="rond-text">
-          <h3 className="hide">TH&#201;S NOIRS</h3>
+          <h3 className="hide">{t("circle.T1")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Thé_vert.avif" alt="" />
         <div className="rond-text">
-          <h3>TH&#201;S VERTS</h3>
+          <h3>{t("circle.T2")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Thé-blanc.avif" alt="" />
         <div className="rond-text">
-          <h3>TH&#201;S BLANCS</h3>
+          <h3>{t("circle.T3")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Thé-jaune.avif" alt="" />
         <div className="rond-text">
-          <h3>TH&#201;S JAUNES</h3>
+          <h3>{t("circle.T4")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Thé-sombre.avif" alt="" />
         <div className="rond-text">
-          <h3>TH&#201;S SOMBRE</h3>
+          <h3>{t("circle.T5")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Thé-oolong.avif" alt="" />
         <div className="rond-text">
-          <h3>TH&#201;S OOLONGS</h3>
+          <h3>{t("circle.T6")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Tisane.avif" alt="" />
         <div className="rond-text">
-          <h3>TISANE</h3>
+          <h3>{t("circle.T7")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Rooibos.avif" alt="" />
         <div className="rond-text">
-          <h3>ROOIBOS</h3>
+          <h3>{t("circle.T8")}</h3>
         </div>
       </div>
       <div className="img">
         <img src="Carcadet.avif" alt="" />
         <div className="rond-text">
-          <h3>CARCADET</h3>
+          <h3>{t("circle.T9")}</h3>
         </div>
       </div>
     </div>
